2ac4e1acbe00ceaa956002798df16f7ba43389e0
sleepinglion <admin@sleepinglion.pe.kr>
2021-02-09 13:48:32 +0900

    수정

--- db/migrate/20201201134915_create_users_questions_answers_custom_answers.rb
+++ db/migrate/20201201134915_create_users_questions_answers_custom_answers.rb
@@ -1,12 +1,10 @@
 class CreateUsersQuestionsAnswersCustomAnswers < ActiveRecord::Migration[6.0]
   def change
     create_table :users_questions_answers_custom_answers do |t|
-      t.references :users_questions_answer, :null=>false
+      t.references :users_questions_answer, :null=>false, index: {unique:true, name: :users_questions_answer_id }
       t.string :title, :null=>false, :limit=>255
       t.boolean :enable, :null=>false, :default=>false
       t.timestamps
       end
-
-      add_index :users_questions_answers_custom_answers, :users_questions_answer, :unique=>true, :name=>:users_questions_answer
   end
 end
