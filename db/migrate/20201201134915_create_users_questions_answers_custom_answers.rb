233cdfc114a5a8054ff807e1f37ede50f8f20cf6
sleepinglion <admin@sleepinglion.pe.kr>
2021-02-09 13:46:37 +0900

    수정

--- db/migrate/20201201134915_create_users_questions_answers_custom_answers.rb
+++ db/migrate/20201201134915_create_users_questions_answers_custom_answers.rb
@@ -5,7 +5,8 @@ class CreateUsersQuestionsAnswersCustomAnswers < ActiveRecord::Migration[6.0]
       t.string :title, :null=>false, :limit=>255
       t.boolean :enable, :null=>false, :default=>false
       t.timestamps
-    end
-#    add_index :answers, [:question_id,:title], :unique=>true, :name=>:answers_unique
+      end
+
+      add_index :users_questions_answers_custom_answers, :users_questions_answer, :unique=>true, :name=>:users_questions_answer
   end
 end
